c7567b9df933a94d317820a67b1f78589bb550cc
Sefa-bulut <sefa603@hotmail.com>
2025-07-19 20:02:00 +0300

    - Sefa: Dosya okuma ve analizi

---
+++ src/java/FileAnalyzer.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Analysis line and character counts for a file.
+ * @author Sefa Bulut
+ */
+public class FileAnalyzer implements Runnable {
+    private int numberOfChars = 0;
+    private int numberOfLines = 0;
+    private File file;
+    private ConcurrentHashMap<String, FileStats> map;
+
+    public FileAnalyzer(File file,ConcurrentHashMap<String, FileStats> map) {
+        this.map = map;
+        this.file = file;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                numberOfLines++;
+                numberOfChars += line.length();
+            }
+
+            // Hesaplanan değerlerin map'e konulması
+            map.put(file.getName(), new FileStats(numberOfChars, numberOfLines));
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
